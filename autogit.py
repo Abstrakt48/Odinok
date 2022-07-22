53e772fb03944c17449627cc7b645874fb6a20d3
abstrakt48 <straightupdirtybass@gmail.com>
2022-07-22 12:11:19 +0100

    being lazy, no proper commit text

--- autogit.py
+++ autogit.py
@@ -1,17 +1,32 @@
 import os
 
+# automatically pushes to github (but you must provide commit text)
+
 def main():
-	print("****** AUTOGIT ******")
+	print("****** AUTOGIT ******\n")
 
-	os.system("git add -A")
-	print("git add -A")
+	be_lazy = input("wanna be lazy?\n0 - no\n1 - yeah\n->")
 
-	commit_text = input("commit text -> ")
+	if be_lazy == 0:
+		os.system("git add -A")
+		print("git add -A")
 
-	print(f"git commit -m '{commit_text}'")
-	os.system(f"git commit -m '{commit_text}'")
-	print("git push")
-	os.system("git push")
+		commit_text = input("commit text -> ")
+
+		print(f"git commit -m '{commit_text}'")
+		os.system(f"git commit -m '{commit_text}'")
+		print("git push")
+		os.system("git push")
+	else:
+		os.system("git add -A")
+		print("git add -A")
+
+		commit_text = "being lazy, no proper commit text"
+
+		print(f"git commit -m '{commit_text}'")
+		os.system(f"git commit -m '{commit_text}'")
+		print("git push")
+		os.system("git push")
 
 if __name__ == '__main__':
 	main()
