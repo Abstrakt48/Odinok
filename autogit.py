05895783941fe1a6de4ddb48902397196c29dca5
abstrakt48 <straightupdirtybass@gmail.com>
2022-07-22 12:06:54 +0100

    testing some autogit stuff

--- autogit.py
+++ autogit.py
@@ -1,12 +1,17 @@
 import os
 
 def main():
-	os.system("git add -A")
+	print("****** AUTOGIT ******")
 
-	commit_text = input("commit text ->")
+	os.system("git add -A")
+	print("git add -A")
+
+	commit_text = input("commit text -> ")
 
 	os.system(f"git commit -m '{commit_text}'")
+	print(f"git commit -m '{commit_text}'")
 	os.system("git push")
+	print("git push")
 
 if __name__ == '__main__':
 	main()
